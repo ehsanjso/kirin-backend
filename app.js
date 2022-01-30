8b71bd8530eee4a6e238a885b2b7543e4d2cd6c2
ehsan.jso <yek.jso@gmail.com>
2022-01-29 22:47:57 -0500

    Add vercel file

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express')
-var path = require('path');
-const port = 3000
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
-const pino = require('pino')
+const express = require("express");
+var path = require("path");
+const port = 4000;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const pino = require("pino");
 const logger = pino({
-  prettyPrint: true
-})
+  prettyPrint: true,
+});
 
-var metricRouter = require('./routes/metrc');
-var activityRouter = require('./routes/activity');
+var metricRouter = require("./routes/metrc");
+var activityRouter = require("./routes/activity");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/metric', metricRouter)
-app.use('/activity', activityRouter)
+app.use("/metric", metricRouter);
+app.use("/activity", activityRouter);
 
 app.listen(port, () => {
-  logger.info(`Example app listening on port ${port}!`)
-})
+  logger.info(`Example app listening on port ${port}!`);
+});
 
 module.exports = app;
